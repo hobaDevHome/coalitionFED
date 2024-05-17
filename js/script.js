8624ad024a207d121d70d4c7823704549f8ee1ca
Heba <40660646+hobaDevHome@users.noreply.github.com>
2024-05-17 23:20:05 +0300

    grid setup

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let password = "skills-test";
   let auth = btoa(`${username}:${password}`);
   let currentObject;
+  let namesList;
 
   // Authenticate (dummy API)
   fetch("https://fedskillstest.coalitiontechnologies.workers.dev", {
@@ -20,10 +21,18 @@ document.addEventListener("DOMContentLoaded", function () {
       throw response;
     })
     .then(function (data) {
-      // console.log("data", data);
       let currentObject = data.find((e) => e.name === "Jessica Taylor");
       displayProfile(currentObject);
-      // console.log("currentObjec2", currentObject2.name);
+
+      namesList = data.map((e) => {
+        return {
+          name: e.name,
+          gender: e.gender,
+          age: e.age,
+        };
+      });
+      console.log("names", namesList);
+      displayUserList(namesList);
     })
     .catch(function (error) {
       console.warn(error);
@@ -41,6 +50,30 @@ document.addEventListener("DOMContentLoaded", function () {
       profilePictureElement.alt = profile.name + "'s Profile Picture";
     } else {
       console.error("One or more elements are missing from the HTML.");
+    }
+  }
+
+  function displayUserList(users) {
+    const userListElement = document.getElementById("userList");
+
+    if (userListElement) {
+      users.forEach((user) => {
+        // Create a new list item for each user
+        const userItem = document.createElement("div");
+        userItem.className = "list-group-item";
+
+        // Add user details
+        userItem.innerHTML = `
+                <h5 class="mb-1">${user.name}</h5>
+                <p class="mb-1">Gender: ${user.gender}</p>
+                <small>Age: ${user.age}</small>
+            `;
+
+        // Append the list item to the user list
+        userListElement.appendChild(userItem);
+      });
+    } else {
+      console.error("The user list element is missing from the HTML.");
     }
   }
 });
